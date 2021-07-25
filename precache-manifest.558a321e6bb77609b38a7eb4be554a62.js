cd9490312394082f43f7f09546e66a756a0e4070
minnuss <ponjevic.goran@hotmail.com>
2021-07-25 18:22:03 +0200

    Updates

---
+++ precache-manifest.558a321e6bb77609b38a7eb4be554a62.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "147e611d1345d3163decae3351d32f87",
+    "url": "/04-Rock-Paper-Scissors/index.html"
+  },
+  {
+    "revision": "a656cf4e32b6f5d72c9a",
+    "url": "/04-Rock-Paper-Scissors/static/css/main.80480653.chunk.css"
+  },
+  {
+    "revision": "38b63845285aa863597e",
+    "url": "/04-Rock-Paper-Scissors/static/js/2.b95c2202.chunk.js"
+  },
+  {
+    "revision": "3405545c67c2b04913629256d4fb5d95",
+    "url": "/04-Rock-Paper-Scissors/static/js/2.b95c2202.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "a656cf4e32b6f5d72c9a",
+    "url": "/04-Rock-Paper-Scissors/static/js/main.440b6878.chunk.js"
+  },
+  {
+    "revision": "340d5df1ca5a58e3126f",
+    "url": "/04-Rock-Paper-Scissors/static/js/runtime-main.5894c119.js"
+  }
+]);
